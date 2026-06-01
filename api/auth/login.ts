2a4a4de5c16bcc885bc85bc27278186b1862b00a
Hazelrust <s-t-a-m-p-2@hotmail.com>
2026-06-01 15:53:29 +0700

    fix: add identify scope to oauth authorize url

--- api/auth/login.ts
+++ api/auth/login.ts
@@ -4,7 +4,7 @@ export default function handler(req: VercelRequest, res: VercelResponse) {
   try {
     const clientId = process.env.VITE_OSU_CLIENT_ID || process.env.OSU_CLIENT_ID;
     const redirectUri = `${req.headers['x-forwarded-proto'] || 'http'}://${req.headers.host}/api/auth/callback`;
-    const authUrl = `https://osu.ppy.sh/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=code&scope=public`;
+    const authUrl = `https://osu.ppy.sh/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=code&scope=public+identify`;
     
     res.redirect(authUrl);
   } catch (err: any) {
